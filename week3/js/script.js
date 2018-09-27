c1ea4c3c3fb2c3870024374db8c010e39cfed23d
aben20807 <aben20807@gmail.com>
2018-09-27 21:07:13 +0800

    Can zoom image by clicking button

--- week3/js/script.js
+++ week3/js/script.js
@@ -2,10 +2,18 @@ var toggle = false;
 
 function btnClick() {
   changeBodyBackground();
+  zoomImage();
   toggle = !toggle;
 }
 
 function changeBodyBackground() {
   document.body.style.background =
     (toggle == 0)? '#617287': '#FCFCFC';
+}
+
+function zoomImage() {
+  document.getElementById('bg').style.width =
+    (toggle == 0)? '110%': '100%';
+  document.getElementById('bg').style.height =
+    (toggle == 0)? '110%': '100%';
 }
