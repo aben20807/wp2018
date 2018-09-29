d30d819cbf6089100e740c374db962166be0aca5
aben20807 <aben20807@gmail.com>
2018-09-29 23:41:14 +0800

    Use jquery to slide

--- week3/js/script.js
+++ week3/js/script.js
@@ -4,6 +4,7 @@ function btnClick() {
   changeBodyBackground();
   changeViewColor();
   zoomImage();
+  slide();
   toggle = !toggle;
 }
 
@@ -22,8 +23,22 @@ function changeViewColor() {
   let view = document.getElementById('view').style;
   view.background = (toggle == 0)? '#617287': '#FCFCFC';
   view.color = (toggle == 1)? '#617287': '#FCFCFC';
-  let read_more = document.getElementsByClassName("read_more");
-  for (var i = 0; i < read_more.length; i++) {
+  let read_more = document.getElementsByClassName('read_more');
+  for (let i = 0; i < read_more.length; i++) {
     read_more[i].style.color = (toggle == 1)? '#617287': '#FCFCFC';
+  }
+}
+
+function slide() {
+  let p1t = $('#p1t');
+  let p2t = $('#p2t');
+  if (!toggle) {
+    p1t.animate({"left":"-100%"}, 1000);
+    p2t.animate({"left":"10%"}, 1000);
+    p1t.animate({"left":"100%"}, 0);
+  } else {
+    p2t.animate({"left":"-100%"}, 1000);
+    p1t.animate({"left":"10%"}, 1000);
+    p2t.animate({"left":"100%"}, 0);
   }
 }
