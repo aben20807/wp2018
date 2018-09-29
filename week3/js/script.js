5e95998a1e96933a8f9be92dfeb5749db6e2bec6
aben20807 <aben20807@gmail.com>
2018-09-29 18:12:25 +0800

    Change text and background color

--- week3/js/script.js
+++ week3/js/script.js
@@ -2,18 +2,28 @@ var toggle = false;
 
 function btnClick() {
   changeBodyBackground();
+  changeViewColor();
   zoomImage();
   toggle = !toggle;
 }
 
 function changeBodyBackground() {
   document.body.style.background =
-    (toggle == 0)? '#617287': '#FCFCFC';
+    (toggle == 0)? '#AB8F82': '#E7E7E7';
 }
 
 function zoomImage() {
-  document.getElementById('bg').style.width =
-    (toggle == 0)? '110%': '100%';
-  document.getElementById('bg').style.height =
-    (toggle == 0)? '110%': '100%';
+  let bg = document.getElementById('bg').style;
+  bg.width = (toggle == 0)? '110%': '100%';
+  bg.height = (toggle == 0)? '110%': '100%';
+}
+
+function changeViewColor() {
+  let view = document.getElementById('view').style;
+  view.background = (toggle == 0)? '#617287': '#FCFCFC';
+  view.color = (toggle == 1)? '#617287': '#FCFCFC';
+  let read_more = document.getElementsByClassName("read_more");
+  for (var i = 0; i < read_more.length; i++) {
+    read_more[i].style.color = (toggle == 1)? '#617287': '#FCFCFC';
+  }
 }
